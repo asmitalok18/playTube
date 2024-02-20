1adf01035cfa4ec31f2e14f7f18209c07d4c5c2d
Asmit Alok <professionalasmitalok@gmail.com>
2024-02-20 23:52:53 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) =>{
     //get user details from frontend
     const {fullName,email,username,password} = req.body
-    console.log("email: ",email);
+    // console.log("email: ",email);
     //validation - not empty
     if(
         [fullName,email,username,password].some((field) =>field?.trim() === "")
@@ -14,16 +14,22 @@ const registerUser = asyncHandler(async (req, res) =>{
         throw new ApiError(400,"All fields are required")
     }
     //check if user already exists: username,email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username},{email}]
     })
 
     if(existedUser){
         throw new ApiError(409,"User with email or username already exist")
     }
+    // console.log(req.files);
     //check for images, check for avatar
    const avatarLocalPath =  req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+//    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
    if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required")
