7602507246d48711389f81de54223ff7299966e9
Asmit Alok <professionalasmitalok@gmail.com>
2024-02-20 10:13:58 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require('dotenv').config({path:'/env'})
 import dotenv from "dotenv"
-
+import { app } from "./app.js";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
